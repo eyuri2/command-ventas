4813b73d4ab54c972c53a144d16a7e66b013d029
Emerson <64048500+eyuri2@users.noreply.github.com>
2020-05-09 23:09:23 -0500

    Add files via upload

---
+++ clients/services.py
@@ -0,0 +1,48 @@
+import csv
+import os
+
+from clients.models import Client
+
+class ClientService:
+    
+    def __init__(self, table_name):
+        self.table_name = table_name
+    
+    def create_client(self, client):
+        with open(self.table_name, mode='a') as f:
+            write = csv.DictWriter(f, fieldnames=Client.schema())
+            write.writerow(client.to_dict())
+    
+    def list_clients(self):
+        with open(self.table_name, mode='r') as f:
+            reader = csv.DictReader(f, fieldnames=Client.schema())
+            
+            return list(reader)
+    
+    def update_clients(self, updated_client):
+        clients = self.list_clients()
+        updated_clients = []
+        for client in clients:
+            if client['uid'] == updated_client.uid:
+                updated_clients.append(updated_client.to_dict())
+            else:
+                updated_clients.append(client)
+        
+        self._save_to_disk(updated_clients)
+    
+    def _save_to_disk(self, clients):
+        tmp_table_name = self.table_name + '.tmp'
+        with open(tmp_table_name, mode='w', newline="") as f:
+            write = csv.DictWriter(f, fieldnames=Client.schema())
+            write.writerows(clients)
+        
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
+    
+    def delete_client(self, client_uid):
+        clients = self.list_clients()
+        for idx,client in enumerate(clients):
+            if client['uid'] == client_uid:
+                clients.pop(idx)
+        
+        self._save_to_disk(clients)
